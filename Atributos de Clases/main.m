27dcf3d1eba90efff5fa53e0fd1231a1fff73cd3
Carlos Castro Brenes <ccasbre27@hotmail.com>
2015-12-10 11:23:15 -0600

    Initial Commit

---
+++ Atributos de Clases/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Atributos de Clases
+//
+//  Created by user on 12/10/15.
+//  Copyright © 2015 user. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
